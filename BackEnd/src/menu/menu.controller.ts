cc16021d9bc86b36542e5e338561e5fa38dcb109
husbanraina <mohammadhusbanraina@gmail.com>
2024-08-13 11:40:31 +0530

    our menu

--- BackEnd/src/menu/menu.controller.ts
+++ BackEnd/src/menu/menu.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
@@ -8,18 +8,56 @@ export class MenuController {
   constructor(private readonly menuService: MenuService) { }
 
 
-  @Post()
+  @Post('item')
   @UseInterceptors(FileInterceptor('file'))
   createItem(@Body() body, @UploadedFile() file: Express.Multer.File,) {
 
     return this.menuService.createItem(body, file)
   }
+  @Get('items')
+  getItems() {
+    return this.menuService.getItems()
+  }
+  @Get('item/:id')
+  getItem(@Param('id') id) {
+    return this.menuService.getItem(id)
+  }
+  @Put('item/:id')
+  @UseInterceptors(FileInterceptor('file'))
+  updateItem(@Param('id') id: string, @Body() body, @UploadedFile() file: Express.Multer.File,) {
+    return this.menuService.updateItem(id, body, file)
+
+  }
+  @Delete('item/:id')
+  deleteItem(@Param('id') id: string) {
+    return this.menuService.deleteItem(id)
+  }
 
 
-  @Post()
+  @Post('category')
   @UseInterceptors(FileInterceptor('file'))
   createCategory(@Body() body, @UploadedFile() file: Express.Multer.File) {
     return this.menuService.createCategory(body, file)
+  }
+
+  @Get('categories')
+  getCategories() {
+    return this.menuService.getCategories()
+  }
+  @Get('category/:id')
+  getCategory(@Param('id') id) {
+    return this.menuService.getCategory(id)
+  }
+
+  @Put('category/:id')
+  @UseInterceptors(FileInterceptor('file'))
+  updateCategory(@Param('id') id: string, @Body() body, @UploadedFile() file: Express.Multer.File,) {
+    return this.menuService.updateCategory(id, body, file)
+
+  }
+  @Delete('category/:id')
+  deleteCategory(@Param('id') id: string) {
+    return this.menuService.deleteCategory(id)
   }
 
 
--- BackEnd/src/menu/menu.service.ts
+++ BackEnd/src/menu/menu.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { MenuCT } from './schema/menu.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -28,6 +28,64 @@ export class MenuService {
 
   }
 
+  async getItems() {
+    const items = await this.menuCTModel.find();
+
+    return items;
+  }
+  async getItem(id) {
+    const item = await this.menuCTModel.findById(id);
+
+    return item;
+  }
+
+  async updateItem(id, body, file) {
+    try {
+
+      // Find user by phone number
+      const item = await this.menuCTModel.findById(id)
+      if (!item) {
+        throw new HttpException('Item not found', HttpStatus.NOT_FOUND);
+      }
+
+      // Update user profile information
+      const updatedItem = await this.menuCTModel.findOneAndUpdate(
+        { _id: id },
+        { $set: body },
+        { new: true } // Return the updated document
+      );
+
+      // If a file is provided, upload the image and set the imageUrl
+      if (file) {
+        const imageUrl = await this.authService.uploadImage(file);
+        updatedItem.item_image_url = imageUrl;
+      }
+
+      // Save the updated user document
+      await updatedItem.save();
+
+      return updatedItem;
+
+    } catch (e) {
+      throw new Error('Failed to update item');
+    }
+
+  }
+
+  async deleteItem(id) {
+    const result = await this.menuCTModel.findOneAndDelete({ _id: id });
+
+    if (!result) {
+      return {
+        message: "Item not found"
+      };
+    }
+
+    return {
+      message: "Item deleted"
+    };
+  }
+
   async createCategory(body, file) {
     const category = await new this.categoryCTModel(body);
 
@@ -40,4 +98,64 @@ export class MenuService {
     await category.save();
 
   }
+
+  async getCategories() {
+    const categories = await this.categoryCTModel.find();
+    return categories;
+  }
+
+  async getCategory(id) {
+    const category = await this.categoryCTModel.findById(id);
+
+    return category;
+  }
+  async updateCategory(id, body, file) {
+    try {
+
+      // Find user by phone number
+      const category = await this.categoryCTModel.findById(id)
+      if (!category) {
+        throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+      }
+
+      // Update user profile information
+      const updatedCategory = await this.categoryCTModel.findOneAndUpdate(
+        { _id: id },
+        { $set: body },
+        { new: true } // Return the updated document
+      );
+
+      // If a file is provided, upload the image and set the imageUrl
+      if (file) {
+        const imageUrl = await this.authService.uploadImage(file);
+        updatedCategory.category_image_url = imageUrl;
+      }
+
+      // Save the updated user document
+      await updatedCategory.save();
+
+      return updatedCategory;
+
+    } catch (e) {
+      throw new Error('Failed to update item');
+    }
+
+  }
+
+  async deleteCategory(id) {
+    const result = await this.categoryCTModel.findOneAndDelete({ _id: id });
+
+
+
+    if (!result) {
+      return {
+        message: "Category not found"
+      };
+    }
+
+    return {
+      message: "Category deleted"
+    };
+  }
+
 }
